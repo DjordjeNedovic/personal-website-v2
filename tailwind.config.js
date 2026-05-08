5ad5d76797e2236c184c8687c97242e148831ce3
Djordje Nedovic <Nedovic.djordje@gmail.com>
2026-05-08 12:21:00 +0200

    Netlixy fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // @ts-check
-const defaultTheme = require('tailwindcss/defaultTheme')
-const fontFamily = defaultTheme.default?.fontFamily
+const { fontFamily } = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
+/** @type {import("tailwindcss/types").Config } */
 module.exports = {
   content: [
     './node_modules/pliny/**/*.js',
